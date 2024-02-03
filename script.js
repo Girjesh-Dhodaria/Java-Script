90aa4f2b8c235258f651ffd5e0e65b0be774374e
GIRJESH DHODARIA <119854398+Girjesh-Dhodaria@users.noreply.github.com>
2024-02-03 23:28:12 +0530

    add script file

---
+++ script.js
@@ -0,0 +1,136 @@
+console.log("Hey I am here for learning");
+
+// var a = 5; // global scope
+// // a = a+1; // var mai hum kar sakte hai
+// var b =6;
+// var c = 'mohan';
+// var _a = "Kunal";
+// var 55a = "Rohan"; // not allowed 
+
+// console.log(c); // for print
+// console.log(a +" "+b);
+// console.log(typeof a, typeof c, typeof _a); // check type
+
+
+/*
+let x = 8 // use it because it is block scope
+{
+    let x = 66;
+    console.log(x)
+}
+console.log(x)
+*/
+
+
+// const a1=5;
+// a1=a1+2; // not allowed because a1 is constant
+// console.log(a1);
+// alert("My name is hacker i will hack you enjoy it.")
+
+// // primitive Data tyoes And Objects
+//Null,Number,String,Symbol,Undefined,Boolean,object
+// let x= "Priynshu bhai"
+// let y = 22;
+// let z = 3.5;
+// const p = true;
+// let q = undefined;
+// let r = null;
+// console.log(typeof x,typeof y,typeof z,
+//     typeof p,typeof q,typeof r)
+
+/*
+let item = {
+    "name": 'Harry',
+    "Job code":200,
+    "is_rich":true
+}
+
+console.log(item);
+// console.log(item.name);
+item.salary = "10crores"
+console.log(item);
+item.salary = '100crores'
+console.log(item);
+*/
+
+// // conditional tutorial (Arithmetic, Assignment, Comparison,
+// Logical ) Operators
+let age = 25;
+let grace = 3;
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age % grace);
+// console.log(age ** grace);
+
+// if (age == 18){  // != also use
+//     console.log('You are eligible for voting');
+// }
+// else{
+//     console.log('Sorry You are not eligible for voting')
+// }
+
+
+// if, else if, else ladder
+/*
+if(age==18){
+    console.log("you can drive")
+}
+else if(age>18){
+    console.log("you already drive");
+}
+else{
+    console.log("you can't drive");
+}
+*/
+
+// let m = 6;
+// let n = 8;
+// let c = m>n?(m-n):(n-m);
+// console.log(c)
+
+
+// // basic for loops
+// let a=10;
+// for (let i = 1; i <= a; i++) {
+//     console.log(a+i);
+// } 
+
+
+// let obj = {
+//     name: 'John',
+//     role: 'Pappu',
+//     company:'londe'
+// }
+// // for in loops it gives you keys
+// for(const key in obj){
+//     const element = obj[key];
+//     console.log(key)
+//     // console.log(key, element)
+// }
+
+// // for print character of kunal
+// for(const c of 'kunal'){
+//     console.log(c);
+// }
+
+// while loop
+// let i =0;
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+ 
+
+
+// do while loop
+// let i = 10;
+// do{
+//     console.log(i)
+//     i++;
+// } while(i<6);
+ 
+
+
+
