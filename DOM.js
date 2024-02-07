ecb21ec003d6654c7cedbcae7a193e0e338a4c54
GIRJESH DHODARIA <119854398+Girjesh-Dhodaria@users.noreply.github.com>
2024-02-07 11:19:01 +0530

    add DOM

---
+++ DOM.js
@@ -0,0 +1,22 @@
+// document.body.firstElementChild; // // first element child
+// document.body.firstElementChild.childNodes; // all nodes
+// document.body.firstElementChild.children; // // element nodes
+// document.body.firstElementChild.children[0];
+// document.body.firstElementChild.children[1];
+// document.body.firstElementChild.children[2];
+// document.body.firstElementChild.children[3].nextElementSibling;
+// document.body.firstElementChild.children[3].previousElementSiblingElementSibling;
+// document.body.firstElementChild.children[3].parentElement;
+
+
+
+// document.querySelector(".container").innerText
+// document.querySelector(".box").innerHTML = "Hey I am kunal" 
+// document.querySelector(".container").outerHTML
+// document.querySelector(".container").tagname
+// document.querySelector(".container").nodename
+// document.querySelector(".box").hasAttribute("style")
+// document.querySelector(".box").getAttribute("style")
+// document.querySelector(".box").setAttribute("style", "display: inline")
+// document.querySelector(".box").removeAttribute("style")
+// document.querySelector(".box").classList.toggle("red")
